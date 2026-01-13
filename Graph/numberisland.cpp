b4f78a1cd7cc159502feb1320706815e8bc9d91f
Ayush Tiwari <iamayush522@gmail.com>
2026-01-14 01:42:45 +0530

    number of the island graph

---
+++ Graph/numberisland.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+class Solution2 {
+public:
+    void bfs(int row, int col,
+             vector<vector<int>> &vis,
+             vector<vector<char>> &grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int,int>> q;
+        q.push({row, col});
+        vis[row][col] = 1;
+
+        int dr[4] = {-1, 0, 1, 0};  // up, right, down, left
+        int dc[4] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = r + dr[i];
+                int ncol = c + dc[i];
+
+                if (nrow >= 0 && nrow < n &&
+                    ncol >= 0 && ncol < m &&
+                    grid[nrow][ncol] == '1' &&
+                    !vis[nrow][ncol]) {
+
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>> &grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    cnt++;
+                    bfs(i, j, vis, grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+
+class Solution {
+public:
+    void bfs(int row, int col,
+             vector<vector<int>> &vis,
+             vector<vector<char>> &grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        vis[row][col] = 1;
+
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for (int delrow = -1; delrow <= 1; delrow++) {
+                for (int delcol = -1; delcol <= 1; delcol++) {
+                    int nrow = r + delrow;
+                    int ncol = c + delcol;
+
+                    if (nrow >= 0 && nrow < n &&
+                        ncol >= 0 && ncol < m &&
+                        grid[nrow][ncol] == '1' &&
+                        !vis[nrow][ncol]) {
+
+                        vis[nrow][ncol] = 1;
+                        q.push({nrow, ncol});
+                    }
+                }
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>> &grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    cnt++;
+                    bfs(i, j, vis, grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<char>> grid(n, vector<char>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    Solution obj;
+    cout << obj.numIslands(grid) << endl;
+    return 0;
+}
